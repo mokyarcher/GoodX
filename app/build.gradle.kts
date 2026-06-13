8c99020573bd395e4b2338901c29b65899c02d39
mokyarcher <mokyarcher@github.com>
2026-06-14 01:57:22 +0800

    release: v0.6.2 - 滑动切标签修复 + 导航优化

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "team.sharex.goodx"
         minSdk = 24
         targetSdk = 36
-        versionCode = 45
-        versionName = "0.6.1"
+        versionCode = 46
+        versionName = "0.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
