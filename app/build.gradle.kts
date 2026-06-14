f2dd270c49a859f21d97a6fa61fd05b3f8f51698
mokyarcher <mokyarcher@github.com>
2026-06-14 18:33:34 +0800

    release: v0.7.0 - 字数限制

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "team.sharex.goodx"
         minSdk = 24
         targetSdk = 36
-        versionCode = 53
-        versionName = "0.6.9"
+        versionCode = 54
+        versionName = "0.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
